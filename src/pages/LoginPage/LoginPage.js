4df4b0ffd8694a4521e570ef2d521dd85d5013f7
Robert Petricevic <robopetricevic@gmail.com>
2020-11-04 20:41:03 +0100

    Formik beggining

--- src/pages/LoginPage/LoginPage.js
+++ src/pages/LoginPage/LoginPage.js
@@ -1,23 +1,57 @@
 import React from "react";
+import { withFormik, Form, Field, ErrorMessage } from "formik";
 
 import styles from "./styles.module.css";
 
 const LoginPage = (props) => {
   return (
     <div className={styles.loginPage}>
-      <form className={styles.loginBox}>
+      <Form className={styles.loginBox}>
         <div className={styles.inputBox}>
-          <label htmlFor="loginEmail">Email</label>
-          <input type="text" name="loginEmail" />
+          <label htmlFor="email">Email</label>
+          <ErrorMessage name="email" />
+          <Field type="text" name="email" />
         </div>
         <div className={styles.inputBox}>
-          <label htmlFor="loginPass">Password</label>
-          <input type="password" name="loginPass" />
+          <label htmlFor="password">Password</label>
+          <ErrorMessage name="password" />
+          <Field type="password" name="password" />
         </div>
         <button className={styles.inputBtn}>Submit</button>
-      </form>
+      </Form>
     </div>
   );
 };
 
-export default LoginPage;
+const FormikLoginPage = withFormik({
+  mapPropsToValues() {
+    return {
+      email: "",
+      password: "",
+    };
+  },
+  handleSubmit(values) {
+    console.log(values);
+  },
+  validate: (values) => {
+    let errors = [];
+
+    if (!values.email) {
+      errors.email = "Email is required";
+    } else if (
+      !/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+        values.email
+      )
+    ) {
+      errors.email = "Email not valid";
+    }
+
+    if (!values.password) {
+      errors.password = "Password is required";
+    } else if (values.password.length < 8) {
+      errors.password = "Password must be at least 8 characters long";
+    }
+  },
+})(LoginPage);
+
+export default FormikLoginPage;
